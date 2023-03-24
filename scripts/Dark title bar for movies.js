4ff12a3f9862067a99310d750200ed2ff116d930
Ed Britton <ed.britton@icloud.com>
2023-03-24 10:03:59 +1000

    Update Dark title bar for movies.js
    Fixed typo

--- scripts/Dark title bar for movies.js
+++ scripts/Dark title bar for movies.js
@@ -3,7 +3,7 @@
 // @description Makes the Safari title bar black. Add and remove your own sites to the match lists.
 // @match       https://www.disneyplus.com/*
 // @version     0.0.1
-// @updateURL   https://raw.githubusercontent.com/edbritton/userscripts/main/scripts/Dark%20title%bar%20for%20movies.js
+// @updateURL   https://raw.githubusercontent.com/edbritton/userscripts/main/scripts/Dark%20title%20bar%20for%20movies.js
 // ==/UserScript==
 
 // Should work forever
